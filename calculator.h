153e057c3afeb8377eaa5c7f2a471f6e323c45ed
Jowayria <Jowayriaelwazeri@aucegypt.edu>
2025-03-25 12:12:09 +0200

    Added header files

---
+++ calculator.h
@@ -0,0 +1,14 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+double add(double a, double b);
+double subtract(double a, double b);
+double multiply(double a, double b);
+double divide(double a, double b);
+double factorial(double n);
+double gcd(double a, double b);
+double lcm(double a, double b);
+double generateRandom(double min, double max);
+
+#endif 
+
